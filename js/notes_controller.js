81ce733aca64d8919c07849ba8967f28f9bbae95
Partha Protim Konwar <parthaprotimkonwar@gmail.com>
2016-06-25 20:09:02 +0530

    updated with Add and Get features for Admin.

--- js/notes_controller.js
+++ js/notes_controller.js
@@ -9,16 +9,23 @@ myApp.controller('myController', ['$scope', '$http', 'commonServices','CONSTANTS
     //setting a default value to the select box
     $scope.peopleOrder = "name";
 
-    //Get all SUBJECTS
-    $scope.getSubjectPromise = commonServices.sendHttpRequest("http://localhost:9000/admin/get/subjects",CONSTANTS.GET_METHOD,null);
-    $scope.getSubjectPromise.success(function (data, status, headers, config) {
-        console.log('Got back a response');
-        console.log(data);
-        $scope.subjectList = data.data;
-    }).error(function (data, status, headers, config) {
-        console.log('AWS DOWN');
-    });
 
+
+    //Get all SUBJECTS
+    $scope.getSubjects = function() {
+        console.log('getSubjects executing');
+
+        $scope.getSubjectPromise = commonServices.sendHttpRequest("http://localhost:9000/admin/get/subjects",CONSTANTS.GET_METHOD,null);
+        $scope.getSubjectPromise.success(function (data, status, headers, config) {
+            console.log('Got back a response');
+            console.log(data);
+            $scope.subjectList = data.data;
+        }).error(function (data, status, headers, config) {
+            console.log('AWS DOWN');
+        });
+    }
+    //Execute GetAllSubjects()
+    $scope.getSubjects();
 
     $scope.getChapters = function() {
         //Get all CHAPTERS
@@ -44,27 +51,41 @@ myApp.controller('myController', ['$scope', '$http', 'commonServices','CONSTANTS
             console.log('AWS DOWN');
         });
     }
-    //static data
-   /* $scope.authors = [
-        {
-            'name' : 'partha1',
-            'occupation' : 'engineer',
-            'company' : 'oracle'
-        },
-        {
-            'name' : 'partha_first2',
-            'occupation' : 'engineer_first',
-            'company' : 'oracle_first'
-        },
-        {
-            'name' : 'partha_second3',
-            'occupation' : 'engineer_second',
-            'company' : 'oracle_second'
-        }
-    ];
 
-	$scope.meraval = function() {
-		return "partha protim konwar";
-	};*/
+    $scope.addSubject = function() {
+        var data = {"subjectName" : $scope.newSubjectName};
+        $scope.addSubjectPromise = commonServices.sendHttpRequest("http://localhost:9000/admin/add/subject",CONSTANTS.POST_METHOD,data);
+        $scope.addSubjectPromise.success(function (data, status, headers, config) {
+            console.log('Got back a response');
+            console.log(data);
+            $scope.getSubjects();
+        }).error(function (data, status, headers, config) {
+            console.log('AWS DOWN');
+        });
+    }
+
+    $scope.addChapters = function() {
+        var data = {"subjectId" : $scope.subject_id, "chapterName" : $scope.newChapterName};
+        $scope.addChapterPromise = commonServices.sendHttpRequest("http://localhost:9000/admin/add/chapter",CONSTANTS.POST_METHOD,data);
+        $scope.addChapterPromise.success(function (data, status, headers, config) {
+            console.log('Got back a response');
+            console.log(data);
+            $scope.getChapters();
+        }).error(function (data, status, headers, config) {
+            console.log('AWS DOWN');
+        });
+    }
+
+    $scope.addQuestionAnswer = function() {
+        var data = {"chapterId" : $scope.chapter_id, "question" : $scope.newQuestion, "answer" : $scope.newAnswer};
+        $scope.addQAPromise = commonServices.sendHttpRequest("http://localhost:9000/admin/add/qa",CONSTANTS.POST_METHOD,data);
+        $scope.addQAPromise.success(function (data, status, headers, config) {
+            console.log('Got back a response');
+            console.log(data);
+            $scope.getQuestionAnswers();
+        }).error(function (data, status, headers, config) {
+            console.log('AWS DOWN');
+        });
+    }
 
 }]);
